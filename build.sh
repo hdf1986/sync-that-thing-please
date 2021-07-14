9f84fbb600612ec299d45bb6c168798e5a95a97e
Hugo David Farji <hi@hugo.farji.me>
2021-07-14 13:10:32 -0300

    Added autosync

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ mkdir -p $HOME/.sync-that-thing-please/bin
 
 if [ -d "$HOME/.sync-that-thing-please/source" ] 
 then
+  cd $HOME/.sync-that-thing-please/source
   git --git-dir $HOME/.sync-that-thing-please/source/.git pull origin master 
 else
   git clone git@github.com:hdf1986/sync-that-thing-please.git $HOME/.sync-that-thing-please/source --depth=1
@@ -13,8 +14,16 @@ cd $HOME/.sync-that-thing-please/source
 
 cargo build --release
 
-cp $HOME/.sync-that-thing-please/source/target/release/sync-that-thing-please $HOME/.sync-that-thing-please/bin
+cp $HOME/.sync-that-thing-please/source/target/release/sync-that-thing-please $HOME/.sync-that-thing-please/bin/sync-that-thing-please
 
-ln -s $HOME/.sync-that-thing-please/bin $HOME/bin 
+ln -s $HOME/.sync-that-thing-please/bin $HOME
 
+if crontab -l
+then
+  (crontab -l  | sed --expression '/sync-that-thing-please/d') && echo "0 * * * * $HOME/.sync-that-thing-please/source/build.sh" | crontab -
+else
+  echo "0 * * * * $HOME/.sync-that-thing-please/source/build.sh" | crontab -
+fi
+
+killall sync-that-thing-please
 $HOME/.sync-that-thing-please/bin/sync-that-thing-please
--- readme.md
+++ readme.md
@@ -1,17 +1,24 @@
 - Folder structure creation & sync [Done]
+- Update of it's own code [Done]
+- AutoUpdate of it's own code [Done]
+- One liner installation [Not started]
+- Automated installation of packages when possible [Not started]
 
 - Autosync of generic dot files
-- Automated installation of packages when possible
 - Configurable backup of configs
 - Autodetection of packages not included in config
 - Git integrated
 - Daemons handling
-- Autoupdate of it's own code
 - Secret avoidance
 - Backup tools
   - Ensure git repos are up-to-date & pushed
   - Review typical folders looking for custom files
+  - Upload entire folders to google drive
 - Sync of /bin executables
 - workspace directories handling
 - Autoclone selected repos
 - Autoinclusion of custom scripts
+-
+
+# Installation
+
